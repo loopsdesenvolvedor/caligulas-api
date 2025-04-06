462d8b2935620e389e6f4c11d5cdf208baa4df91
Elizeu <loopsdesenvolvedor@gmail.com>
2025-04-06 02:32:14 -0300

    feat: add next auth settings

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import Goggle from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+import NextAuth, { AuthOptions } from "next-auth";
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Goggle({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
+};
+
+export default NextAuth(authOptions);
